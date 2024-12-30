e83e6fe8c99d87f0f648315a2e3e02df06c74521
Nadongkyu <kaineus86@gmail.com>
2024-12-30 09:31:33 +0900

    Core Initialize

---
+++ core/src/test/java/com/pluxity/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluxity.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
