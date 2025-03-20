6c47930745f9831c0a56a4b9c54253e2dcfa04d5
Na <nadk@pluxity.com>
2025-03-20 16:37:37 +0900

    Hotfix : S3 경로 변경

--- common/src/main/java/com/pluxity/global/config/S3Config.java
+++ common/src/main/java/com/pluxity/global/config/S3Config.java
@@ -8,21 +8,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${aws.s3.bucket}")
+    @Value("${file.s3.bucket}")
     private String bucketName;
 
-    @Value("${aws.s3.region}")
+    @Value("${file.s3.region}")
     private String region;
 
-    @Value("${aws.s3.endpoint-url}")
+    @Value("${file.s3.endpoint-url}")
     private String endpointUrl;
 
-    @Value("${aws.s3.access-key}")
+    @Value("${file.s3.access-key}")
     private String accessKey;
 
-    @Value("${aws.s3.secret-key}")
+    @Value("${file.s3.secret-key}")
     private String secretKey;
 
-    @Value("${aws.s3.pre-signed-url-expiration}")
+    @Value("${file.s3.pre-signed-url-expiration}")
     private int preSignedUrlExpiration;
 }
