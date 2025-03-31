15e94c6b13a32479ed8485bfa57ff4f16dbf62c0
Na <nadk@pluxity.com>
2025-03-31 18:33:05 +0900

    이니셜라이즈

---
+++ project-busan-subway/src/main/java/com/pluxity/ProjectBusanSubway.java
@@ -0,0 +1,11 @@
+package com.pluxity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBusanSubway {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBusanSubway.class, args);
+    }
+}
