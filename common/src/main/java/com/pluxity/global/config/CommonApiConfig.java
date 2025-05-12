09fea2d977421dbece40bde20b504f2b420cc9a7
Na <nadk@pluxity.com>
2025-05-12 17:14:50 +0900

    Swagger 그룹화

--- common/src/main/java/com/pluxity/global/config/CommonApiConfig.java
+++ common/src/main/java/com/pluxity/global/config/CommonApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,5 +26,61 @@ public class CommonApiConfig {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+
+    @Bean
+    public GroupedOpenApi commonApi() {
+        return GroupedOpenApi.builder()
+                .group("1. 전체")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+
+    @Bean
+    public GroupedOpenApi authApi() {
+        return GroupedOpenApi.builder()
+                .group("2. 인증")
+                .pathsToMatch("/auth/**")
+                .pathsToExclude("/users/**", "/admin/**", "/other/**") // 제외 경로 추가
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi fileApiByPath() {
+        return GroupedOpenApi.builder()
+                .group("3. 파일관리 API")
+                .pathsToMatch("/files/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userApiByPath() {
+        return GroupedOpenApi.builder()
+                .group("4. 사용자 API")
+                .pathsToMatch("/users/**", "/admin/users/**", "/roles/**")
+                .build();
+    }
+
+
+    @Bean
+    public GroupedOpenApi facilityApiByPath() {
+        return GroupedOpenApi.builder()
+                .group("5. 시설관리 API")
+                .pathsToMatch(
+                        "/facilities/**",
+                        "/buildings/**",
+                        "/stations/**",
+                        "/panoramas/**"
+                )
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi assetApiByPath() {
+        return GroupedOpenApi.builder()
+                .group("6. Asset 관리 API")
+                .pathsToMatch("/assets/**", "/asset-sets/**")
+                .build();
     }
 } 
