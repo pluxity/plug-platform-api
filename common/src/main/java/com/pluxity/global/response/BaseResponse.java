e3833870b78e5b8d11a0ba0eb002b1438cf35563
Na <nadk@pluxity.com>
2025-05-13 17:26:02 +0900

    Hotfix : BaseResponse 추가

---
+++ common/src/main/java/com/pluxity/global/response/BaseResponse.java
@@ -0,0 +1,23 @@
+package com.pluxity.global.response;
+
+import com.pluxity.global.entity.BaseEntity;
+
+public record BaseResponse(
+        String createdAt,
+        String createdBy,
+        String updatedAt,
+        String updatedBy
+) {
+
+    public static BaseResponse of(BaseEntity entity) {
+        return new BaseResponse(
+                entity.getCreatedAt().toString(),
+                entity.getCreatedBy(),
+                entity.getUpdatedAt().toString(),
+                entity.getUpdatedBy()
+        );
+    }
+
+
+
+}
