177da4c411ec12767f4664ad285400207404c978
Na <nadk@pluxity.com>
2025-05-23 13:23:49 +0900

    hotfix(clean-code): 코드 정리

--- core/src/main/resources/db/migration/V4__add_default_role.sql
+++
@@ -1,9 +0,0 @@
--- ADMIN 역할 추가 (이미 'ADMIN' 이름의 역할이 존재하면 아무것도 하지 않음)
-INSERT INTO roles (name, description, created_at, updated_at)
-VALUES ('ADMIN', 'System Administrator role created by Flyway migration', NOW(), NOW())
-ON CONFLICT (name) DO NOTHING;
-
--- USER 역할 추가 (이미 'USER' 이름의 역할이 존재하면 아무것도 하지 않음)
-INSERT INTO roles (name, description, created_at, updated_at)
-VALUES ('USER', 'Default User role created by Flyway migration', NOW(), NOW())
-ON CONFLICT (name) DO NOTHING;
