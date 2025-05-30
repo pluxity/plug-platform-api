4ddddb7303a14eb3fa9a491c606ebc0205698a6b
Lay <kmr1993@pluxity.com>
2025-05-30 10:28:36 +0900

    api features -> with-features 변경 features는 상위 레벨로 변경

--- sasang/src/main/java/com/pluxity/domains/station/dto/SasangStationCreateRequest.java
+++ sasang/src/main/java/com/pluxity/domains/station/dto/SasangStationCreateRequest.java
@@ -1,6 +1,7 @@
 package com.pluxity.domains.station.dto;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.pluxity.facility.station.dto.StationCreateRequest;
 
 public record SasangStationCreateRequest(
-        StationCreateRequest stationRequest, String externalCode) {}
+        @JsonUnwrapped StationCreateRequest stationRequest, String externalCode) {}
