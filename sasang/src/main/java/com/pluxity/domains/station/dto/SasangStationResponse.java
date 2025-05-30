fedb718013e363464eecf0d76f0fc92a50510b89
Lay <kmr1993@pluxity.com>
2025-05-30 14:45:21 +0900

    feature -> featureIds

--- sasang/src/main/java/com/pluxity/domains/station/dto/SasangStationResponse.java
+++ sasang/src/main/java/com/pluxity/domains/station/dto/SasangStationResponse.java
@@ -3,15 +3,16 @@ package com.pluxity.domains.station.dto;
 import com.pluxity.facility.facility.dto.FacilityResponse;
 import com.pluxity.facility.floor.dto.FloorResponse;
 import com.pluxity.facility.station.dto.StationResponse;
-import java.util.List;
 import lombok.Builder;
+
+import java.util.List;
 
 @Builder
 public record SasangStationResponse(
         FacilityResponse facility,
         List<FloorResponse> floors,
         List<Long> lineIds,
-        List<String> features,
+        List<String> featureIds,
         String route,
         String externalCode) {
 
@@ -20,7 +21,7 @@ public record SasangStationResponse(
                 .facility(stationResponse.facility())
                 .floors(stationResponse.floors())
                 .lineIds(stationResponse.lineIds())
-                .features(stationResponse.featureIds())
+                .featureIds(stationResponse.featureIds())
                 .route(stationResponse.route())
                 .externalCode(externalCode)
                 .build();
