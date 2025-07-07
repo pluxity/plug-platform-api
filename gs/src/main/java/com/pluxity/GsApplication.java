bc24ced22459cda6fd8778ef3abd41fe1930fc51
Lay <kmr1993@pluxity.com>
2025-07-07 16:35:55 +0900

    GS init

---
+++ gs/src/main/java/com/pluxity/GsApplication.java
@@ -0,0 +1,11 @@
+package com.pluxity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GsApplication.class, args);
+  }
+}
