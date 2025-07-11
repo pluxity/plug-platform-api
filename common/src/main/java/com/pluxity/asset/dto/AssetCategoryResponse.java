e35b13e189512873154837932c9a6c1e771d8b33
moon <mym1207@pluxity.com>
2025-07-11 10:15:48 +0900

    fix: swagger 카테고리 자식 example이 string으로 표시되는 문제수정
    (cherry picked from commit cb44c830bb61f932326088abea77349d90f06cb2)

--- common/src/main/java/com/pluxity/asset/dto/AssetCategoryResponse.java
+++ common/src/main/java/com/pluxity/asset/dto/AssetCategoryResponse.java
@@ -13,7 +13,11 @@ public record AssetCategoryResponse(
         @Schema(description = "카테고리 이름", example = "그래픽 에셋") String name,
         @Schema(description = "카테고리 코드", example = "GRAPHIC_ASSET") String code,
         @Schema(description = "부모 카테고리 ID", example = "2") Long parentId,
-        @Schema(description = "자식 카테고리 목록") List<AssetCategoryResponse> children,
+        @Schema(
+                        description = "자식 카테고리 목록",
+                        example =
+                                "[{\"id\":2,\"name\":\"서브 카테고리\",\"code\":\"SUB\",\"parentId\":1,\"children\":[],\"thumbnail\":null,\"assetIds\":[0],\"createdAt\":\"string\",\"updatedAt\":\"string\",\"depth\":2}]")
+                List<AssetCategoryResponse> children,
         @Schema(description = "아이콘 파일 정보") FileResponse thumbnail,
         @Schema(description = "소속 에셋 ID 목록") List<Long> assetIds,
         @Schema(description = "생성일시") LocalDateTime createdAt,
--- common/src/main/java/com/pluxity/facility/category/dto/FacilityCategoryResponse.java
+++ common/src/main/java/com/pluxity/facility/category/dto/FacilityCategoryResponse.java
@@ -10,7 +10,11 @@ public record FacilityCategoryResponse(
         @Schema(description = "카테고리 ID", example = "1") Long id,
         @Schema(description = "카테고리 이름", example = "그래픽 에셋") String name,
         @Schema(description = "부모 카테고리 ID", example = "2") Long parentId,
-        @Schema(description = "자식 카테고리 목록") List<FacilityCategoryResponse> children,
+        @Schema(
+                        description = "자식 카테고리 목록",
+                        example =
+                                "[{\"id\":2,\"name\":\"서브 카테고리\",\"parentId\":1,\"children\":[],\"createdAt\":\"string\",\"updatedAt\":\"string\",\"depth\":2}]")
+                List<FacilityCategoryResponse> children,
         @Schema(description = "생성일시") LocalDateTime createdAt,
         @Schema(description = "수정일시") LocalDateTime updatedAt,
         @Schema(description = "depth") int depth) {
