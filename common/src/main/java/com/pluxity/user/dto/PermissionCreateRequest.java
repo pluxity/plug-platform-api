9990dd91655f656d27f6c6fa3b1bd9bd56fc64c0
moon <mym1207@pluxity.com>
2025-07-29 14:59:17 +0900

    fix: swagger 문서 ResourceType 타입 제한 추가

--- common/src/main/java/com/pluxity/user/dto/PermissionCreateRequest.java
+++ common/src/main/java/com/pluxity/user/dto/PermissionCreateRequest.java
@@ -5,5 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record PermissionCreateRequest(
-        @NotNull @Schema(description = "권한을 관리할 리소스의 타입", example = "FACILITY") String resourceName,
+        @NotNull
+                @Schema(
+                        description = "권한을 관리할 리소스의 타입",
+                        example = "FACILITY",
+                        allowableValues = {"FACILITY", "BUILDING", "PARK", "DEVICE"})
+                String resourceName,
         @NotBlank @Schema(description = "권한을 부여/관리할 리소스의 ID", example = "101") String resourceId) {}
--- common/src/main/java/com/pluxity/user/dto/PermissionUpdateRequest.java
+++ common/src/main/java/com/pluxity/user/dto/PermissionUpdateRequest.java
@@ -3,5 +3,9 @@ package com.pluxity.user.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record PermissionUpdateRequest(
-        @Schema(description = "변경할 리소스의 타입", example = "FACILITY") String resourceName,
+        @Schema(
+                        description = "변경할 리소스의 타입",
+                        example = "FACILITY",
+                        allowableValues = {"FACILITY", "BUILDING", "PARK", "DEVICE"})
+                String resourceName,
         @Schema(description = "변경할 리소스의 ID", example = "102") String resourceId) {}
