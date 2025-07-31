1d107f0f9e209974ce73284935e523fe90a93a8d
Lay <kmr1993@pluxity.com>
2025-07-31 11:27:32 +0900

    fix: PermissionResponse 에서 BaseResponse 제거, PermissionRequest resourceType을 enum으로 표출하도록 수정

--- common/src/main/java/com/pluxity/permission/dto/PermissionRequest.java
+++ common/src/main/java/com/pluxity/permission/dto/PermissionRequest.java
@@ -1,9 +1,12 @@
 package com.pluxity.permission.dto;
 
+import com.pluxity.permission.ResourceType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record PermissionRequest(
-        @NotNull @Schema(description = "자원 유형") String resourceType,
+        @NotNull
+                @Schema(description = "자원 유형", implementation = ResourceType.class, example = "FACILITY")
+                String resourceType,
         @NotNull @Schema(description = "자원 아이디") List<String> resourceIds) {}
--- common/src/main/java/com/pluxity/permission/dto/PermissionResponse.java
+++ common/src/main/java/com/pluxity/permission/dto/PermissionResponse.java
@@ -1,16 +1,10 @@
 package com.pluxity.permission.dto;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
-import com.pluxity.global.response.BaseResponse;
 import com.pluxity.permission.Permission;
 
-public record PermissionResponse(
-        Long id, String resourceName, String resourceId, @JsonUnwrapped BaseResponse baseResponse) {
+public record PermissionResponse(Long id, String resourceName, String resourceId) {
     public static PermissionResponse from(Permission permission) {
         return new PermissionResponse(
-                permission.getId(),
-                permission.getResourceName(),
-                permission.getResourceId(),
-                BaseResponse.of(permission));
+                permission.getId(), permission.getResourceName(), permission.getResourceId());
     }
 }
