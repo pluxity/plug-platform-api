6577a0d8a417352200538c8df309bf7b3fd2a0a8
moon <mym1207@pluxity.com>
2025-08-13 11:06:51 +0900

    fix: 코틀린버전 1.9.25로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlinVersion = "2.2.0"
+    val kotlinVersion = "1.9.25"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     id("org.springframework.boot") version "3.5.4"
