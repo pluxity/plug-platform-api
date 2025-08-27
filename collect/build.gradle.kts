57f5e288cd2265950e750160965eb77046ecbe78
moon <mym1207@plyxity.com>
2025-08-27 16:59:52 +0900

    fix: 불필요한 설정 제거

--- collect/build.gradle.kts
+++ collect/build.gradle.kts
@@ -1,6 +1,3 @@
-tasks.named("bootJar") { enabled = true }
-tasks.jar { enabled = true }
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
