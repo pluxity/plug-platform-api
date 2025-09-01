dec85ff7f986790554521b59935eb2ff8aa7a969
moon <mym1207@plyxity.com>
2025-09-01 12:48:31 +0900

    feat: DeviceService Kotest 추가

--- common/src/testFixtures/kotlin/device/DummyEntities.kt
+++ common/src/testFixtures/kotlin/device/DummyEntities.kt
@@ -1,6 +1,10 @@
 package device
 
+import com.pluxity.device.entity.Device
 import com.pluxity.device.entity.DeviceCategory
+import com.pluxity.device.entity.DeviceCompanyType
+import com.pluxity.device.entity.DeviceType
+import com.pluxity.feature.entity.Feature
 
 fun dummyDeviceCategory(
     name: String = "category-name",
@@ -10,3 +14,12 @@ fun dummyDeviceCategory(
 ).apply {
     updateName(name)
 }
+
+fun dummyDevice(
+    id: String = "device_id",
+    name: String = "device_name",
+    feature: Feature? = null,
+    category: DeviceCategory? = dummyDeviceCategory(iconFileId = 1L),
+    deviceType: DeviceType = DeviceType.TEMP_HUM,
+    companyType: DeviceCompanyType = DeviceCompanyType.DAWONDNS,
+) = Device(id, name, feature, category, deviceType, companyType)
