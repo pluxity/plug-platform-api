b98c090f4ab48bc978b1c9af40cfc5cd46952f13
moon <mym1207@plyxity.com>
2025-09-05 10:46:42 +0900

    refactor: makeCategoryTree 개선

--- common/src/main/java/com/pluxity/global/utils/MappingUtils.kt
+++ common/src/main/java/com/pluxity/global/utils/MappingUtils.kt
@@ -1,28 +1,22 @@
 package com.pluxity.global.utils
 
-import java.util.function.Function
-
 object MappingUtils {
     fun <T> makeCategoryTree(
         list: List<T>,
-        idGetter: Function<T, Long?>,
-        parentIdGetter: Function<T, Long?>,
-        childrenGetter: Function<T, MutableList<T>>,
+        idGetter: (T) -> Long?,
+        parentIdGetter: (T) -> Long?,
+        childrenGetter: T.() -> MutableList<T>,
     ): List<T> {
         val map =
             list
                 .mapNotNull { item ->
-                    idGetter.apply(item)?.let { id -> id to item }
+                    idGetter(item)?.let { id -> id to item }
                 }.toMap()
         val roots = mutableListOf<T>()
-        for (item in list) {
-            val parentId = parentIdGetter.apply(item)
-            if (parentId == null) {
-                roots.add(item)
-            } else {
-                map[parentId]?.let { parent ->
-                    childrenGetter.apply(parent).add(item)
-                }
+        list.forEach { item ->
+            when (val parentId = parentIdGetter(item)) {
+                null -> roots += item
+                else -> map[parentId]?.childrenGetter()?.add(item)
             }
         }
         return roots
