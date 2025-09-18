bea0a3a702f4e4ac6933545977ce6454f4b53dc7
Lay <kmr1993@pluxity.com>
2025-09-18 11:16:14 +0900

    fix: spotlessApply

--- common/src/main/kotlin/com/pluxity/user/entity/User.kt
+++ common/src/main/kotlin/com/pluxity/user/entity/User.kt
@@ -101,7 +101,7 @@ class User(
         resourceId: String,
     ): Boolean =
         userRoles.any { it.role.name == RoleType.ADMIN.roleName } ||
-                userRoles.any { it.role.hasPermissionFor(resourceName, resourceId) }
+            userRoles.any { it.role.hasPermissionFor(resourceName, resourceId) }
 
     fun isPasswordChangeRequired(): Boolean =
         lastPasswordChangeDate.isBefore(
