2ffd5dbe1f53cfa72094717ff74b35ed4047f9f0
inyeop <kim41516@pluxity.com>
2025-12-05 13:10:40 +0900

    onboarding: @Transactional 추가 및 save 로직 수정

--- core/src/main/kotlin/com/pluxity/onboarding/service/OnboardingFacilityService.kt
+++ core/src/main/kotlin/com/pluxity/onboarding/service/OnboardingFacilityService.kt
@@ -5,6 +5,7 @@ import com.pluxity.facility.FacilityRepository
 import com.pluxity.onboarding.dto.OnboardingFacilityRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class OnboardingFacilityService @Autowired constructor(
@@ -13,22 +14,21 @@ class OnboardingFacilityService @Autowired constructor(
 ){
     private val prefix = "onboarding/"
 
+    @Transactional
     fun save(
         facility: Facility,
         request: OnboardingFacilityRequest
     ): Facility {
-        // facility 저장
-        facility.updateThumbnailFileId(request.thumbnailId)
-
+        // facility를 저장
         val savedFacility = facilityRepository.save(facility)
 
-        val filePath = "$prefix${savedFacility.id}/"
-        // 썸네일 file 저장
         request.thumbnailId?.let { thumbnailId ->
-            fileService.persistFile(thumbnailId, filePath)
-            facility.updateThumbnailFileId(thumbnailId)
-        }
+            val filePath = "$prefix${savedFacility.id!!}/"
 
+            fileService.persistFile(thumbnailId, filePath)
+
+            savedFacility.updateThumbnailFileId(thumbnailId)
+        }
         return savedFacility
     }
 
