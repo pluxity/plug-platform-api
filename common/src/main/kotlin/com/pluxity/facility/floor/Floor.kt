3e843904c17e7e9ff2f70e81440e14918e3364bd
inyeop <kim41516@pluxity.com>
2026-01-08 11:44:57 +0900

    refactor: Floor 클래스 IdentityIdEntity 상속 구조로 변경

--- common/src/main/kotlin/com/pluxity/facility/floor/Floor.kt
+++ common/src/main/kotlin/com/pluxity/facility/floor/Floor.kt
@@ -1,12 +1,10 @@
 package com.pluxity.facility.floor
 
 import com.pluxity.facility.Facility
+import com.pluxity.global.entity.IdentityIdEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
@@ -24,8 +22,4 @@ class Floor(
     val floorId: String,
     @Column(name = "name", nullable = false)
     val name: String,
-) {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
-}
+) : IdentityIdEntity()
