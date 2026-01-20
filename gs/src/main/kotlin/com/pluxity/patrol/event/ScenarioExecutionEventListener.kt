e4168471a6443814b570c8d8852623e1fb050257
inyeop <kim41516@pluxity.com>
2026-01-20 16:26:25 +0900

    feat: AsyncAPI 애너테이션 추가

--- gs/src/main/kotlin/com/pluxity/patrol/event/ScenarioExecutionEventListener.kt
+++ gs/src/main/kotlin/com/pluxity/patrol/event/ScenarioExecutionEventListener.kt
@@ -1,6 +1,9 @@
 package com.pluxity.patrol.event
 
 import com.pluxity.messaging.dto.ScenarioTriggerMessage
+import io.github.springwolf.bindings.stomp.annotations.StompAsyncOperationBinding
+import io.github.springwolf.core.asyncapi.annotations.AsyncOperation
+import io.github.springwolf.core.asyncapi.annotations.AsyncPublisher
 import org.slf4j.LoggerFactory
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.scheduling.annotation.Async
@@ -20,6 +23,15 @@ class ScenarioExecutionEventListener(
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @AsyncPublisher(
+        operation =
+            AsyncOperation(
+                channelName = TOPIC_SCENARIO_EXECUTION,
+                description = "시나리오 트리거 발생 시 클라이언트에게 알림",
+                payloadType = ScenarioTriggerMessage::class,
+            ),
+    )
+    @StompAsyncOperationBinding
     fun handle(event: ScenarioExecutedEvent) {
         log.info("시나리오 실행 이벤트 처리: {}", event)
         messagingTemplate.convertAndSend(
