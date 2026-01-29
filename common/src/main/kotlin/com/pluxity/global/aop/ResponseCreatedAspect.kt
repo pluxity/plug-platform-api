3c8c8ff6b2ab79e06e6fd2be0e20f41c3a59bbda
moon <>
2026-01-29 11:05:46 +0900

    feat: ResponseCreatedAspect facilityId 추가 추출 추가

--- common/src/main/kotlin/com/pluxity/global/aop/ResponseCreatedAspect.kt
+++ common/src/main/kotlin/com/pluxity/global/aop/ResponseCreatedAspect.kt
@@ -4,9 +4,11 @@ import com.pluxity.global.annotation.ResponseCreated
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.reflect.MethodSignature
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import java.net.URI
+import kotlin.collections.indexOf
 
 @Aspect
 @Component
@@ -18,6 +20,26 @@ class ResponseCreatedAspect {
     ): ResponseEntity<Void> {
         val result = joinPoint.proceed() as ResponseEntity<*>
         val id = result.getBody()
-        return ResponseEntity.created(URI.create(responseCreated.path.replace("{id}", id?.toString() ?: ""))).build()
+        val facilityId = getArgument(joinPoint, "facilityId") // 추가 추출
+        return ResponseEntity
+            .created(
+                URI.create(
+                    responseCreated.path
+                        .replace("{id}", id?.toString() ?: "")
+                        .replace("{facilityId}", facilityId?.toString() ?: ""),
+                ),
+            ).build()
+    }
+
+    private fun getArgument(
+        joinPoint: ProceedingJoinPoint,
+        name: String,
+    ): Any? {
+        val signature = joinPoint.signature as MethodSignature
+        val paramNames = signature.parameterNames
+        val args = joinPoint.args
+
+        val index = paramNames.indexOf(name)
+        return if (index >= 0) args[index] else null
     }
 }
